5b2aab30bf864536c0b496fdc996081fa58b58de
Diz1111Diz <kavunv@gmail.com>
2020-04-05 00:23:22 +0300

    Updates

---
+++ precache-manifest.9f9852a1573c4013fdec0e1b1489c9e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6484d0281f8f6bdc25d1007e4f41874c",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "c694a4f306f9dc5e55cb",
+    "url": "/goit-react-hw-01-components/static/css/main.52c2861c.chunk.css"
+  },
+  {
+    "revision": "e26918cb394b5b5907f7",
+    "url": "/goit-react-hw-01-components/static/js/2.d478d694.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.d478d694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c694a4f306f9dc5e55cb",
+    "url": "/goit-react-hw-01-components/static/js/main.35de57b9.chunk.js"
+  },
+  {
+    "revision": "61ea0416005e4a073e74",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.60be8746.js"
+  }
+]);
